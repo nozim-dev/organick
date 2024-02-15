7712876dbd47f2a3aad07d060b24643a64941976
nozim-dev <nozimfordevelop@gmail.com>
2024-02-15 16:15:26 +0500

    i change name position

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 function App() {
   return (
     <>
+      <h1>Hello my names Asror</h1>
       <h1 className="text-3xl text-green-300 font-bold underline">
         Hello world!
       </h1>
       <h1 className="text-blue-700">hello i'm Javohir </h1>
       <h1>Hello Javokhir I'm a Teacher</h1>
-      <h1>Hello my names Asror</h1>
     </>
   );
 }
