d14f1cc03967dac6ffaee47b9eb708ef9adefa9f
nozim-dev <nozimfordevelop@gmail.com>
2024-02-20 16:20:58 +0500

    adding notfound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <Header />
+      NotFound page
+      <Footer />
+    </div>
+  );
 };
 
 export default NotFound;
